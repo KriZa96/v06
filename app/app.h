660dce174c3a36b5d5ae493f9b20630d4f15cfbe
KriZa96 <kriza9605@gmail.com>
2024-12-19 20:22:09 +0100

    Vjezba 6 rijesena

--- app/app.h
+++ app/app.h
@@ -2,5 +2,31 @@
 
 namespace vsite::oop::v6 
 {
+	template <typename T>
+	T minimum(const T a, const T b) {
+		return a < b ? a : b;
+	}
 
+	template <typename T>
+	struct point {
+		T x;
+		T y;
+	};
+
+	template <typename T, unsigned long long N>
+	class fixed_array {
+	public:
+		unsigned long long size() const {
+			return N;
+		}
+
+		T& operator[](const unsigned long long index)
+		{
+			return arr[index];
+		}
+
+	private:
+		T arr[N];
+		unsigned long long size_;
+	};
 }
